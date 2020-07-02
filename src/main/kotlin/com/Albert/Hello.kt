bf09c7ae2884334782ee3ab7c9198bc30356694f
AlbertLeong <ngcasi22@gmail.com>
2020-07-02 14:30:24 +0800

    Initial commit

---
+++ src/main/kotlin/com/Albert/Hello.kt
@@ -0,0 +1,14 @@
+package com.Albert
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val h = Human()
+    h.hello()
+
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
